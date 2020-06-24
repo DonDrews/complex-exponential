a2fb18508a036e03145bafa9fe60a811877cf688
DonDrews <donovancarldrews@gmail.com>
2020-06-24 13:19:22 -0700

    Rearranged some code and added arrow rendering.

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var canvas;
 var ctx;
-const lineGap = 100;
+const lineGap = 75;
+var w;
+var h;
+var colors = ['#FF0000', '#00BB00', '#0000FF'];
 
 function draw()
 {
@@ -15,10 +18,17 @@ function draw()
 
 function pixelToCoord(x, y)
 {
-    mathX = (x - (w / 2)) / (lineGap * 2);
-    mathI = -(y - (h / 2)) / (lineGap * 2);
+    var mathX = (x - (w / 2)) / (lineGap * 2);
+    var mathI = -(y - (h / 2)) / (lineGap * 2);
 
     return [mathX, mathI];
+}
+
+function coordToPixel(r, i)
+{
+    var screenX = r * (lineGap * 2) + w / 2
+    var screenY = -i * (lineGap * 2) + h / 2;
+    return [screenX, screenY];
 }
 
 function sign(number)
@@ -29,19 +39,66 @@ function sign(number)
         return '+' + number.toFixed(2);
 }
 
+function factorial(k)
+{
+    if(k === 0)
+        return 1;
+    else
+        return k * factorial(k - 1);
+}
+
+function getTerm(z_r, z_i, k)
+{
+    var theta = Math.atan2(z_i, z_r);
+    var radius = Math.sqrt(Math.pow(z_r, 2) + Math.pow(z_i, 2));
+
+    //using DeMoivre's theorem
+    theta = theta * k;
+    radius = Math.pow(radius, k) / factorial(k);
+
+    var r = Math.cos(theta) * radius;
+    var i = Math.sin(theta) * radius;
+    return [r, i];
+}
+
 function dispfunc(e)
 {
+    drawGraph();
+    drawArrow(w / 2, h / 2, e.clientX, e.clientY, '#000000', 2, true);
+
+    var [x, i] = pixelToCoord(e.clientX, e.clientY);
+
+    var lastR = 0;
+    var lastI = 0;
+    for(var n = 0; n < 15; n++)
+    {
+        var [termR, termI] = getTerm(x, i, n);
+        var [dispX, dispY] = coordToPixel(termR + lastR, termI + lastI);
+        var [origX, origY] = coordToPixel(lastR, lastI);
+        if(n < 2)
+            drawArrow(origX, origY, dispX, dispY, colors[n % 3], 1, true);
+        else
+            drawArrow(origX, origY, dispX, dispY, colors[n % 3], 1, false);
+        lastR += termR;
+        lastI += termI;
+    }
+
+    drawUI(x, i);
+}
+
+//always called last
+function drawUI(x, i)
+{
+    drawBoxes();
     const math = document.getElementById("textual");
 
-    [x, i] = pixelToCoord(e.clientX, e.clientY);
-
-    fullExp = '$$e^{' + sign(x) + sign(i) + '}=';
+    //contruct TeX syntax for showing partial sum
+    fullExp = '$$e^{' + sign(x) + sign(i) + 'i}=';
     fullExp += '1+(' + sign(x) + sign(i) + 'i)';
     
     for(var n = 2; n < 5; n++)
     {
         fullExp += '+\\frac{';
-        //remove the plus based on sign
         fullExp += '(' + sign(x) + sign(i);
 
         fullExp += 'i)^' + n;
@@ -54,7 +111,6 @@ function dispfunc(e)
     // display and re-render the expression
     MathJax.Hub.Queue(['Typeset', MathJax.Hub, math])
 }
-
 
 function drawGraph()
 {
@@ -70,7 +126,7 @@ function drawGraph()
 
     //draw all the graph lines
     ctx.strokeStyle = '#A0A0A0'
-    for(var x = -10; x < 10; x++)
+    for(var x = -14; x < 14; x++)
     {
         if(x === 0)
             continue;
@@ -79,10 +135,9 @@ function drawGraph()
         ctx.beginPath();
         ctx.moveTo(xPos, 0);
         ctx.lineTo(xPos, h);
-        ctx.closePath();
         ctx.stroke();
     }
-    for(var y = -5; y < 5; y++)
+    for(var y = -8; y < 8; y++)
     {
         if(y === 0)
             continue;
@@ -91,14 +146,12 @@ function drawGraph()
         ctx.beginPath();
         ctx.moveTo(0, yPos);
         ctx.lineTo(w, yPos);
-        ctx.closePath();
         ctx.stroke();
     }
 
     //draw the unit circle
     ctx.beginPath();
     ctx.arc((w / 2), (h / 2), lineGap * 2, 0, 2 * Math.PI, true);
-    ctx.closePath();
     ctx.stroke();
 
     //draw the central axes
@@ -107,13 +160,11 @@ function drawGraph()
     ctx.beginPath();
     ctx.moveTo((w / 2) - 0.3, 0);
     ctx.lineTo((w / 2) - 0.3, h);
-    ctx.closePath();
     ctx.stroke();
 
     ctx.beginPath();
     ctx.moveTo(0, (h / 2) - 0.3);
     ctx.lineTo(w, (h / 2) - 0.3);
-    ctx.closePath();
     ctx.stroke();
 
     //draw the axis numbers
@@ -145,4 +196,39 @@ function drawBoxes()
 {
     ctx.fillStyle = '#000000B0';
     ctx.fillRect(290, 15, 1000, 80);
+}
+
+function drawArrow(x, y, toX, toY, color, size, head)
+{
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    
+    var len = Math.sqrt(Math.pow(toX - x, 2) + Math.pow(toY - y, 2));
+    var backX = ((toX - x) / len) * 10 * size;
+    var backY = ((toY - y) / len) * 10 * size;
+    var corner1X = toX - backX + (backY * 0.2);
+    var corner1Y = toY - backY + (-backX * 0.2);
+    var corner2X = toX - backX + (-backY * 0.2);
+    var corner2Y = toY - backY + (backX * 0.2);
+    
+
+    //draw arrow shaft
+    ctx.lineWidth = size;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    if(head)
+        ctx.lineTo(toX - backX, toY - backY);
+    else
+        ctx.lineTo(toX, toY);
+    ctx.stroke();
+
+    if(head)
+    {
+        //draw arrow head
+        ctx.beginPath();
+        ctx.moveTo(toX, toY);
+        ctx.lineTo(corner1X, corner1Y);
+        ctx.lineTo(corner2X, corner2Y);
+        ctx.fill();
+    }
 }
